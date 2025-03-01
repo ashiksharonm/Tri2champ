a4f3c4bbd74efeefbee1aba10ec44ffaeaadc1c6
ashiksharonm <ashiksharon.24cs@licet.ac.in>
2025-03-01 23:02:26 +0530

    Security update

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,16 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { getAuth } from "firebase/auth";
 
-export const firebaseConfig = {
-    apiKey: "AIzaSyB_mIscj9j6Dqyf3ujlqfgbP1JakEygD68",
-    authDomain: "tri2champ.firebaseapp.com",
-    databaseURL: "https://tri2champ-default-rtdb.firebaseio.com",
-    projectId: "tri2champ",
-    storageBucket: "tri2champ.appspot.com",
-    messagingSenderId: "318844875234",
-    appId: "1:318844875234:web:c76a1750ee286f168e89bb",
-    measurementId: "G-VV8HWE99L3"
-  };
-  
-  export const app = initializeApp(firebaseConfig);
-  export const auth = getAuth(app);
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
