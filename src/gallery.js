53d89e31bde1a65d877b7025c8a8acb9422537cd
ashiksharonm <ashiksharon.24cs@licet.ac.in>
2025-03-01 22:55:56 +0530

    Security update

--- src/gallery.js
+++ src/gallery.js
@@ -2,44 +2,50 @@ import React, { useEffect, useState } from "react";
 import "./gallery.css";
 import Footer from "./footer";
 import { Helmet } from "react-helmet";
-import { initializeApp } from 'firebase/app';
-import firebase from 'firebase/compat/app';
+import { initializeApp } from "firebase/app";
+import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
-import { getStorage, ref, uploadBytes, listAll, getDownloadURL } from "firebase/storage";
+import {
+  getStorage,
+  ref,
+  uploadBytes,
+  listAll,
+  getDownloadURL,
+} from "firebase/storage";
 import NavbarM from "./nav2";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB_mIscj9j6Dqyf3ujlqfgbP1JakEygD68",
-  authDomain: "tri2champ.firebaseapp.com",
-  databaseURL: "https://tri2champ-default-rtdb.firebaseio.com",
-  projectId: "tri2champ",
-  storageBucket: "tri2champ.appspot.com",
-  messagingSenderId: "318844875234",
-  appId: "1:318844875234:web:c76a1750ee286f168e89bb",
-  measurementId: "G-VV8HWE99L3",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 const storage = getStorage(app);
 const storageRef = ref(storage);
-const imagesRef = ref(storageRef, 'images');
+const imagesRef = ref(storageRef, "images");
 
 const HomeGallery = () => {
   const [images, setImages] = useState([]);
 
   useEffect(() => {
     listAll(imagesRef)
-      .then(res => {
-        const imagePromises = res.items.map(item => getDownloadURL(item));
+      .then((res) => {
+        const imagePromises = res.items.map((item) => getDownloadURL(item));
         Promise.all(imagePromises)
-          .then(urls => {
+          .then((urls) => {
             setImages(urls);
           })
-          .catch(error => {
+          .catch((error) => {
             console.error(error);
           });
       })
-      .catch(error => {
+      .catch((error) => {
         console.error(error);
       });
   }, []);
@@ -58,7 +64,7 @@ const HomeGallery = () => {
       </Helmet>
       <NavbarM />
       <div className="masonry-container">
-        {images.map(url => (
+        {images.map((url) => (
           <div key={url} className="masonry-item">
             <img src={url} alt="uploaded" className="hhov" />
           </div>
